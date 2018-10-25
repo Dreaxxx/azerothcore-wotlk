f444274d31ca33cacc52f97fbdff6c06efc63d3d
Dreaxx <lecarp@hotmail.fr>
2018-10-25 15:10:09 +0200

    Update Val'Kyr
    Ball distance from player less large

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_twin_valkyr.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_twin_valkyr.cpp
@@ -874,8 +874,8 @@ class spell_valkyr_ball_periodic_dummy : public SpellScriptLoader
                 if (Unit* target = GetTarget())
                     if (target->GetDisplayId() != 11686)
                         if (Creature* me = target->ToCreature())
-                            if (Player* p = me->SelectNearestPlayer(2.5f))
-                                if (me->GetExactDist2d(p) <= 2.5f)
+                            if (Player* p = me->SelectNearestPlayer(2.0f))
+                                if (me->GetExactDist2d(p) <= 2.0f)
                                 {
                                     me->AI()->DoAction(1); // despawning = true;
                                     me->GetMotionMaster()->MoveIdle();
